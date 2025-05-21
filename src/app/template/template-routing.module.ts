10f94be79119d964bf9f2ec844585c53d29596ae
Alendre Maciel Barros de Melo <alendrek6@hotmail.com>
2025-05-21 20:35:22 -0300

    feat: Criando a estrutura do módulo e componente de galeria

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
         loadChildren:() => import('../lugares/lugares.module').then(m => m.LugaresModule),
         pathMatch: 'full'
 
+      },{
+      path: 'galeria',
+      loadChildren:() => import('../galeria/galeria.module').then(m => m.GaleriaModule),
+      pathMatch: 'full'
       }
     ]
   }
