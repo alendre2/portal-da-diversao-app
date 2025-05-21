10699e610bfb6276588e7558028cc73a3271bf9a
Alendre Maciel Barros de Melo <alendrek6@hotmail.com>
2025-05-21 19:39:23 -0300

    feat: Adicionando os campos do formulário de lugares

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Brinquedos } from '../../brinquedos/brinquedos';
 
 @Component({
   selector: 'app-lugar',
@@ -8,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class LugarComponent {
 
+  camposForm : FormGroup;
+  categorias: Brinquedos[]=[]
+
+  constructor(){
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliaca: new FormControl('', Validators.required)
+
+    })
+  }
+
+  salvar(){
+    console.log('Valores', this.camposForm.value)
+  }
 }
